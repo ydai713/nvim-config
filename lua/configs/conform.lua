eb90ea50f8c1076652f0f8a5b0e55f244ebc0800
ydai713 <yang.dai@klaviyo.com>
2024-03-18 00:33:09 -0700

    update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,9 +1,15 @@
 local options = {
-  lsp_fallback = true,
+	lsp_fallback = true,
 
-  formatters_by_ft = {
-    lua = { "stylua" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		javascript = { { "prettierd", "prettier" } },
+		typescript = { { "prettierd", "prettier" } },
+		javascriptreact = { { "prettierd", "prettier" } },
+		typescriptreact = { { "prettierd", "prettier" } },
+		terraform = { "terraform_fmt" },
+	},
 }
 
 require("conform").setup(options)
