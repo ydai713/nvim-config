9aa60f7357d357d459a414122fdab48cbd1cb05e
Yang Dai <yang.dai@klaviyo.com>
2024-03-24 16:02:03 -0700

    format

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,16 +1,16 @@
 local options = {
-	lsp_fallback = true,
+  lsp_fallback = true,
 
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "black" },
-		javascript = { { "prettierd", "prettier" } },
-		typescript = { { "prettierd", "prettier" } },
-		javascriptreact = { { "prettierd", "prettier" } },
-		typescriptreact = { { "prettierd", "prettier" } },
-		terraform = { "terraform_fmt" },
-		sql = { "sqlfluff" },
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = { "black" },
+    javascript = { { "prettierd", "prettier" } },
+    typescript = { { "prettierd", "prettier" } },
+    javascriptreact = { { "prettierd", "prettier" } },
+    typescriptreact = { { "prettierd", "prettier" } },
+    terraform = { "terraform_fmt" },
+    sql = { "sqlfluff" },
+  },
 }
 
 require("conform").setup(options)
